0fa45096b128722d6920470cf14a279ddc2cf926
Alex Parog <sbibik2000@mail.ru>
2025-01-10 17:38:10 +0300

    init bank-api-service

---
+++ bank-api-service/src/main/java/ru/abibik/bankapiservice/BankApiServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.abibik.bankapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankApiServiceApplication.class, args);
+    }
+
+}
